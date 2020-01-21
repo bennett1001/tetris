763a2ec6465695c280bc20d050870083fe49b74c
bennett1001 <60077999+bennett1001@users.noreply.github.com>
2020-01-20 19:53:24 -0500

    Update Main.py

--- Tetris/Main.py
+++ Tetris/Main.py
@@ -21,8 +21,8 @@ pygame.font.init()
 # GLOBALS VARS
 s_width = 800
 s_height = 700
-play_width = 300  # meaning 300 // 10 = 30 width per block
-play_height = 600  # meaning 600 // 20 = 20 height per block
+play_width = 300  # 300 // 10 = 30 width per block
+play_height = 600  # 600 // 20 = 20 height per block
 block_size = 30
 
 top_left_x = (s_width - play_width) // 2
